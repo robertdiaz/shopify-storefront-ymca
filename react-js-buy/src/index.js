73697ef87fd462d5e3b0fa36095b2aafaf2cce9d
robertdiaz <diaz.creative@gmail.com>
2019-03-26 01:47:05 -0400

    Update index.js

--- react-js-buy/src/index.js
+++ react-js-buy/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import Client from 'shopify-buy';
 import '../../shared/app.css';
+import './app.css';
+import './ymca-custom.css';
 
 const client = Client.buildClient({
-  storefrontAccessToken: 'dd4d4dc146542ba7763305d71d1b3d38',
-  domain: 'graphql.myshopify.com'
+  storefrontAccessToken: '0a60f2891c94681317d6927daf08992a',
+  domain: 'adaptive-media.myshopify.com'
 });
 
 ReactDOM.render(
